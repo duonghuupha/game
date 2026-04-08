26d35731c042cc518b5fb31a70d927bb887e117e
duonghuupha <duonghuupha@hotmail.com.vn>
2026-04-08 16:56:15 +0700

    8

--- api/get-answers.php
+++ api/get-answers.php
@@ -1,14 +1,6 @@
 <?php
-
 require "../db.php";
-
 $game = $db->query("select * from game")->fetch();
-
 $qid = $game['current_question'];
-
-$q = $db->query("
-select * from answers 
-where question_id='$qid'
-")->fetchAll();
-
+$q = $db->query("select * from answers where question_id='$qid'")->fetchAll();
 echo json_encode($q);
--- api/get-game.php
+++ api/get-game.php
@@ -1,7 +1,4 @@
 <?php
-
 require "../db.php";
-
 $g = $db->query("select * from game")->fetch();
-
 echo json_encode($g);
